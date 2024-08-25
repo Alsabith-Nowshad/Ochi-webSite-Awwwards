de3a570f34363d459bbaed3f588d0dc91487632e
Alsabith-Nowshad <alsabithnowshad123@gmail.com>
2024-08-25 11:27:34 +0530

    day-5

--- app/Components/About.jsx
+++ app/Components/About.jsx
@@ -1,24 +1,54 @@
-
-import React, { useEffect } from 'react'
+"use  client"
+import React, { useEffect, useState } from 'react';
+import { GoArrowUpRight } from "react-icons/go";
 
 const About = () => {
 
+
+    const [hover,sethover] = useState(false)
+
+    let enter = () =>{
+      
+      sethover(true)
+
+    }
+
+    let leave = () =>{
+      
+      sethover(false)
+
+    }
+
+
+
+
   return (
-    <div className='overflow-hidden py-20 bg-[#CDEA68] rounded-tl-[20px] rounded-tr-[20px] text-[#000] px-[5vh] leading-[50px]'>
-      <h1 className='text-[30px] md:text-[50px]'>Ochi is a strategic presentation agency for forward-thinking businesses that need to raise funds, sell prod­ucts, ex­plain com­plex ideas, and hire great peo­ple.</h1>
+    <div className="overflow-hidden py-20 bg-[#CDEA68] rounded-tl-[20px] rounded-tr-[20px] text-[#000] px-[5vh] leading-[50px]">
+      <h1 className="text-[30px] md:text-[50px]">
+        Ochi is a strategic presentation agency for forward-thinking businesses
+        that need to raise funds, sell prod­ucts, ex­plain com­plex ideas, and
+        hire great peo­ple.
+      </h1>
       <div className="w-full lg:flex gap-5 border-t-[1px] mt-20 border-[#71b563]">
-        <div className='w-1/2'>
-        <h1 className='text-[20px] 2xl:text-[70px] pt-10'>Our apporoch:</h1>
-        <button className=' bg-zinc-900 rounded-full uppercase text-[8px] px-10 py-2 md:text-[15px] md:py-2 md:px-8  mt-10 text-white flex gap-2  items-center sm:px-14 sm:py-4 sm:gap-10'>Read More
-            <div className='w-1 h-1 md:w-2 md:h-2 bg-zinc-100 rounded-full '></div>
-        </button>
+        <div className="w-1/2">
+          <h1 className="text-[20px] 2xl:text-[70px] pt-10">Our apporoch:</h1>
+          <button onMouseEnter={enter} onMouseLeave={leave} className="btn w-[238px] h-[80px] bg-zinc-900 rounded-full uppercase px-10 py-2 text-[15px]  mt-10 text-white flex gap-8 items-center">
+            Read More
+            <div className={`bg-zinc-100 rounded-full transition-all duration-300 ease-in-out  text-black flex justify-center items-center ${hover ? 'w-10 h-10 text-[23px]' : 'w-2 h-2 text-transparent'} `}>
+            <  GoArrowUpRight />
+            </div>
+          </button>
         </div>
         <div className="w-[70vw] h-[45vw] lg:w-[870px] lg:h-[630px] mt-10 rounded-[15px] bg-[#afc950]">
-        <img className='w-[70vw] h-[45vw] lg:w-[870px] lg:h-[630px]  rounded-[15px]' src="./About.jpg" alt="" /></div>
+          <img
+            className="w-[70vw] h-[45vw] lg:w-[870px] lg:h-[630px]  rounded-[15px]"
+            src="./About.jpg"
+            alt=""
+          />
         </div>
+      </div>
     </div>
-   
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- app/Components/Eyes.jsx
+++ app/Components/Eyes.jsx
@@ -1,47 +1,58 @@
-"use client"
-import React, { useEffect, useState } from 'react'
-
+'use client';
+import React, { useEffect, useState } from 'react';
 
 const Eyes = () => {
-  const [rotate, setRotate] = useState(0)
+  const [rotate, setRotate] = useState(0);
 
-useEffect(()=>{
-    window.addEventListener("mousemove",(e)=>{
-     let mouseX =   e.clientX 
-    let mouseY = e.clientY
+  useEffect(() => {
+    window.addEventListener('mousemove', (e) => {
+      let mouseX = e.clientX;
+      let mouseY = e.clientY;
 
-    let deltaX = mouseX - window.innerWidth/2
-    let deltaY = mouseY - window.innerHeight/2
+      let deltaX = mouseX - window.innerWidth / 2;
+      let deltaY = mouseY - window.innerHeight / 2;
 
-    var angle = Math.atan2(deltaY,deltaX) * (180/Math.PI)
-        setRotate(angle-180)
-    })
-})
-
-
+      var angle = Math.atan2(deltaY, deltaX) * (180 / Math.PI);
+      setRotate(angle - 180);
+    });
+  });
 
   return (
-    <div className='eyes w-full h-screen overflow-hidden'>
-      <div data-scroll data-scroll-speed="-.7" className="relative w-[100vw] h-[100vh] sm:w-full sm:h-full bg-cover bg-center bg-[url(https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-scaled.jpg)]">
-      <div className="absolute  left-[50%] top-[50%] -translate-x-[50%] -translate-y-[50%] flex gap-10 ">
-      <div className="w-[150px]  h-[150px] sm:w-[250px]  sm:h-[250px] rounded-full bg-zinc-100 flex items-center justify-center">
-                <div className='relative w-[100px] h-[100px]  sm:w-[200px] sm:h-[200px] rounded-full bg-zinc-900 flex items-center justify-center'>
-                    <div style={{transform: `translate(-50%, -50%) rotate(${rotate}deg)`}} className="line  absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] line  w-full h-10 ">
-                     <div className='w-[40px] h-[40px] rounded-full bg-zinc-100'></div>
-                     </div>
-                </div>
-      </div>
-      <div className="w-[150px]  h-[150px] sm:w-[250px]  sm:h-[250px] rounded-full bg-zinc-100 flex items-center justify-center">
-      <div className='relative w-[100px] h-[100px]  sm:w-[200px] sm:h-[200px] rounded-full bg-zinc-900 flex items-center justify-center'>
-                    <div style={{transform: `translate(-50%, -50%) rotate(${rotate}deg)`}} className="line  absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] line   w-full h-10 ">
-                     <div className='w-[40px] h-[40px] rounded-full bg-zinc-100'></div>
-                     </div>
-                </div>
-      </div>
-      </div>
+    <div className="eyes w-full h-screen overflow-hidden">
+      <div
+        data-scroll
+        data-scroll-speed="-.7"
+        className="relative w-[100vw] h-[100vh] sm:w-full sm:h-full bg-cover bg-center bg-[url(https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-scaled.jpg)]"
+      >
+        <div className="absolute  left-[50%] top-[50%] -translate-x-[50%] -translate-y-[50%] flex gap-10 ">
+          <div className="w-[150px]  h-[150px] sm:w-[250px]  sm:h-[250px] rounded-full bg-zinc-100 flex items-center justify-center">
+            <div className="relative w-[100px] h-[100px]  sm:w-[200px] sm:h-[200px] rounded-full bg-zinc-900 flex items-center justify-center">
+              <div
+                style={{
+                  transform: `translate(-50%, -50%) rotate(${rotate}deg)`,
+                }}
+                className="line  absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] line  w-full h-10 "
+              >
+                <div className="w-[40px] h-[40px] rounded-full bg-zinc-100"></div>
+              </div>
+            </div>
+          </div>
+          <div className="w-[150px]  h-[150px] sm:w-[250px]  sm:h-[250px] rounded-full bg-zinc-100 flex items-center justify-center">
+            <div className="relative w-[100px] h-[100px]  sm:w-[200px] sm:h-[200px] rounded-full bg-zinc-900 flex items-center justify-center">
+              <div
+                style={{
+                  transform: `translate(-50%, -50%) rotate(${rotate}deg)`,
+                }}
+                className="line  absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] line   w-full h-10 "
+              >
+                <div className="w-[40px] h-[40px] rounded-full bg-zinc-100"></div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Eyes
+export default Eyes;
